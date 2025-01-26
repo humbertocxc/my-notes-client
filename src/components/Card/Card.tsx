bfd5caaa6cae7dae13297906339a0a139e4a8ec8
humberto <humberto.gnm98@gmail.com>
2025-01-26 14:02:51 -0300

    refactor (Card component)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,9 +2,9 @@ import { Box, Button, Checkbox, Typography } from "@mui/material"
 import { Delete } from "@mui/icons-material";
 import { useMutation } from "@apollo/client";
 import { Draggable } from "@hello-pangea/dnd";
-import { Status } from "../typedefs/task/types";
-import { changeTaskInfoMutation, IChangeTaskInfo } from "../typedefs/task/changeTaskInfo";
-import { deleteTaskMutation, IDeleteTask } from "../typedefs/task/deleteTask";
+import { Status } from "../../typedefs/task/types";
+import { changeTaskInfoMutation, IChangeTaskInfo } from "../../typedefs/task/changeTaskInfo";
+import { deleteTaskMutation, IDeleteTask } from "../../typedefs/task/deleteTask";
 
 export interface CardProps {
   id: string
